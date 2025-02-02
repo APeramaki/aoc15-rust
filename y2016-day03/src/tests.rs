118882aabee56414424c58013b258adbd4982ae9
Atte Perämäki <atte-90@gmail.com>
2025-02-02 22:17:11 +0200

    year 2016 day 03 init

---
+++ y2016-day03/src/tests.rs
@@ -0,0 +1,12 @@
+use super::*;
+#[test]
+fn part1() {
+    let example_input = "5 10 25\n15 25 12";
+    assert_eq!(solve_part2(example_input), 1)
+}
+
+#[test]
+fn part2() {
+    let example_input = "5 10 25\n15 25 12";
+    assert_eq!(solve_part2(example_input), 0);
+}
