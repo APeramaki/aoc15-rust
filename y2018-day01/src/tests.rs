3bc05c438804c2d8df69ff1e387d37fc1a4e0449
Atte Perämäki <atte-90@gmail.com>
2025-02-13 22:50:55 +0200

    y2018d01 test format fix

--- y2018-day01/src/tests.rs
+++ y2018-day01/src/tests.rs
@@ -1,11 +1,11 @@
 use super::*;
 #[test]
 fn part1() {
-    let example_input = "+1, +1, +1";
+    let example_input = "+1\n+1\n1";
     assert_eq!(solve_part1(example_input), 3);
-    let example_input = "+1, +1, -2 ";
+    let example_input = "+1\n+1\n-2 ";
     assert_eq!(solve_part1(example_input), 0);
-    let example_input = "-1, -2, -3";
+    let example_input = "-1\n-2\n-3";
     assert_eq!(solve_part1(example_input), -6);
 }
 
