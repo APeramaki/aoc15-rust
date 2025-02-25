12e6eaf6e2b3bd0e6102a47af705713abd04ae5a
Atte Perämäki <atte-90@gmail.com>
2025-02-25 23:18:10 +0200

    02/2018 init

---
+++ y2018-day02/src/tests.rs
@@ -0,0 +1,11 @@
+use super::*;
+#[test]
+fn part1() {
+    let example_input = "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab";
+    assert_eq!(solve_part1(example_input), 12);
+}
+
+#[test]
+fn part2() {
+    todo!()
+}
