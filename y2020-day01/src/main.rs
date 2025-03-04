21736fd917e88d10d9053c428adbe97610b22f6c
Atte Perämäki <atte-90@gmail.com>
2025-03-04 23:08:25 +0200

    01/2020 part 1 done

--- y2020-day01/src/main.rs
+++ y2020-day01/src/main.rs
@@ -1,5 +1,19 @@
+use std::collections::HashSet;
+const TARGET: u32 = 2020;
+
 fn solve_part1(input: &str) -> u32 {
-    todo!()
+    let mut found: HashSet<u32> = HashSet::new();
+    input
+        .lines()
+        .map(|line| line.parse::<u32>().unwrap())
+        .find_map(|value| {
+            if let Some(x) = found.get(&(TARGET - value)) {
+                return Some(x * value);
+            }
+            found.insert(value);
+            None
+        })
+        .unwrap()
 }
 
 fn solve_part2(input: &str) -> u32 {
@@ -10,7 +24,7 @@ fn main() {
     use std::time::Instant;
     let now = Instant::now();
 
-    let input = std::fs::read_to_string("inputs/y2015-day08.txt").expect("Failed to read input");
+    let input = std::fs::read_to_string("inputs/y2020-day01.txt").expect("Failed to read input");
     let result = solve_part1(&input);
     println!(
         "Part 1 solution: {}, time taken {:.2?}",
--- y2020-day01/src/tests.rs
+++ y2020-day01/src/tests.rs
@@ -11,3 +11,15 @@ fn part1() {
 "#;
     assert_eq!(solve_part1(example_input), 514_579);
 }
+
+#[test]
+fn part2() {
+    let example_input = r#"1721
+979
+366
+299
+675
+1456
+"#;
+    assert_eq!(solve_part2(example_input), 241_861_950);
+}
