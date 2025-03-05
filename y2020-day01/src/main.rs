e1fb1b65582e494663f085653de5b4b53b83ca82
Atte Perämäki <atte-90@gmail.com>
2025-03-05 13:08:45 +0200

    01/2020 part 2 done

--- y2020-day01/src/main.rs
+++ y2020-day01/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 const TARGET: u32 = 2020;
 
 fn solve_part1(input: &str) -> u32 {
@@ -17,7 +17,29 @@ fn solve_part1(input: &str) -> u32 {
 }
 
 fn solve_part2(input: &str) -> u32 {
-    todo!()
+    // For single values read
+    let mut values: Vec<u32> = Vec::new();
+    // Each possible pair of numbers. Key: Pair sum, Value: Pair product
+    let mut pairs: HashMap<u32, u32> = HashMap::new();
+
+    input
+        .lines()
+        .map(|line| line.parse::<u32>().unwrap())
+        .find_map(|value| {
+            // Key should be TARGET - current value
+            if let Some(v) = pairs.get(&(TARGET - value)) {
+                return Some(v * value);
+            }
+            // Store all pairs
+            values.iter().for_each(|v| {
+                pairs.insert(v + value, v * value);
+            });
+            // Add current value to list
+            values.push(value);
+
+            None
+        })
+        .unwrap()
 }
 
 fn main() {
