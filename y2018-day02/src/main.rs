d3b5217c4e3c8fcc6a58825f549230fe24a93033
Atte Perämäki <atte-90@gmail.com>
2025-04-01 01:32:59 +0300

    02/2018 part 1 quick and dirty

--- y2018-day02/src/main.rs
+++ y2018-day02/src/main.rs
@@ -1,7 +1,29 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn solve_part1(input: &str) -> i32 {
-    todo!()
+    let mut doubles = 0;
+    let mut triples = 0;
+    input.lines().for_each(|line| {
+        let mut count: HashMap<char, u8> = HashMap::new();
+        line.chars().for_each(|c| {
+            count.entry(c).and_modify(|value| *value += 1).or_insert(1);
+        });
+
+        let (had_double, had_triple) =
+            count
+                .iter()
+                .fold((0, 0), |(has_double, has_triple), (_, v)| {
+                    if *v == 2 {
+                        return (1, has_triple);
+                    } else if *v == 3 {
+                        return (has_double, 1);
+                    }
+                    (has_double, has_triple)
+                });
+        doubles += had_double;
+        triples += had_triple;
+    });
+    doubles * triples
 }
 
 fn solve_part2(input: &str) -> i32 {
